d2ecb5e3457d76ab28b81ddd7aae12b83303baa7
JefersonPinn <113300091+JefersonPinn@users.noreply.github.com>
2022-09-23 22:56:17 -0300

    Routes To admin
    Create a routes containing Register and Login(with JWT)

---
+++ src/routes/admin.js
@@ -0,0 +1,49 @@
+const Admin = require("../models/admin");
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const check = require("../middleware/check");
+const jwt = require("jsonwebtoken");
+
+//criar admin
+router.post('/register', async (req, res)=>{
+
+    try {
+        //generate new password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    
+        //create new admin
+        const newAdmin = new Admin({
+          username: req.body.username,
+          email: req.body.email,
+          password: hashedPassword
+        });
+    
+        //save user and respond
+        const admin = await newAdmin.save();
+        res.status(201).json(admin);
+      } catch {(err) => {
+        res.status(500).json(err) }
+      }
+    });
+
+
+//LOGIN admin
+router.post("/login", async (req, res) => {
+    try {
+      const admin = await admin.findOne({ email: req.body.email });
+      !admin && res.status(404).json("Não Autorizado");
+  
+      const validPassword = await bcrypt.compare(req.body.password, admin.password)
+      !validPassword && res.status(401).json("Não Autorizado");
+      
+      const token = await jwt.sign({email:admin.email}, 'segredo', {expiresIn: 84600});
+
+      admin && res.status(200).json(token);
+    } catch {
+      (err) => {
+      res.status(401).json(err) }
+    }
+  });
+
+module.exports = router;
