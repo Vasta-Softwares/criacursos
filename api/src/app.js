07d60807954bce16baaef97f9e1f69d122045343
JefersonPinn <113300091+JefersonPinn@users.noreply.github.com>
2022-10-09 21:03:22 -0300

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -10,8 +10,8 @@ const app = express();
 const port = 3800
 
 //conectando o mongoose a base de dados
-const DB_USER = 'criacursos';
-const DB_PASSWORD = 'criacursos123';
+const DB_USER = '';
+const DB_PASSWORD = '';
 mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@criacursos.9yjy8ci.mongodb.net/?retryWrites=true&w=majority`)
 .then(()=>{
     console.log('conectamos ao MONGODB')
